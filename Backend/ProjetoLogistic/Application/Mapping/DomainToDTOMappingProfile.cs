e8017606d695195c0426062c53e827d95719622f
arthur-cabral <arthurmartins10052002@gmail.com>
2023-09-21 02:00:44 -0300

    feat: Creating AddInfrastructure IService and implementing AutoMapper

---
+++ Backend/ProjetoLogistic/Application/Mapping/DomainToDTOMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTO.Company;
+using Application.DTO.Sale;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mapping
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Company, CompanyDTO>().ReverseMap();
+            CreateMap<Sale, SaleDTO>().ReverseMap();
+        }
+    }
+}
---
+++ Backend/ProjetoLogistic/CrossCutting/IoC/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using Application.Interfaces;
+using Application.Mapping;
+using Application.Services;
+using Domain.Interfaces;
+using Infrastructure.Context;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossCutting.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseMySql(configuration.GetConnectionString("DefaultConnection"),
+                new MySqlServerVersion(new Version(1, 1)), b => b.MigrationsAssembly("API")));
+
+            services.AddScoped<ICompanyService, CompanyService>();
+            
+            services.AddScoped<ICompanyRepository, CompanyRepository>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            return services;
+        }
+    }
+}

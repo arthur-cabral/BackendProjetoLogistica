5c616328c13a61162665421bbcc10b3ae165277c
arthur-cabral <arthurmartins10052002@gmail.com>
2023-09-21 18:00:18 -0300

    chore: Adding pagination in project

--- Backend/ProjetoLogistic/Application/Mapping/DomainToDTOMappingProfile.cs
+++ Backend/ProjetoLogistic/Application/Mapping/DomainToDTOMappingProfile.cs
@@ -1,7 +1,9 @@
 ﻿using Application.DTO.Company;
+using Application.DTO.Pagination;
 using Application.DTO.Sale;
 using AutoMapper;
 using Domain.Entities;
+using Domain.Pagination;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +18,7 @@ namespace Application.Mapping
         {
             CreateMap<Company, CompanyDTO>().ReverseMap();
             CreateMap<Sale, SaleDTO>().ReverseMap();
+            CreateMap<PaginationParameters, PaginationParametersDTO>().ReverseMap();
         }
     }
 }

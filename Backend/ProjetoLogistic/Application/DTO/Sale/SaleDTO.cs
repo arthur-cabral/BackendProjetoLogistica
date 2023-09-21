affd14a4f1d142d74844909103006c55865e62c6
arthur-cabral <arthurmartins10052002@gmail.com>
2023-09-21 02:05:58 -0300

    feat: Creating Sale entity and SaleDTO

---
+++ Backend/ProjetoLogistic/Application/DTO/Sale/SaleDTO.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTO.Company;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO.Sale
+{
+    public class SaleDTO
+    {
+        public long Id { get; set; }
+        public string Description { get; set; }
+        public double Value { get; set; }
+        public DateTime DateCreated { get; set; }
+        public CompanyDTO Company { get; set; }
+    }
+}

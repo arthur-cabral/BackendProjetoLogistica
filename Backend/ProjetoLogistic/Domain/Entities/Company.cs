92586ff01d0211f6907139544347f1753acf5132
arthur-cabral <arthurmartins10052002@gmail.com>
2023-09-22 03:16:00 -0300

    chore: Adding ef annotations in Company and Sale entities

--- Backend/ProjetoLogistic/Domain/Entities/Company.cs
+++ Backend/ProjetoLogistic/Domain/Entities/Company.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +10,17 @@ namespace Domain.Entities
 {
     public class Company
     {
+        public Company()
+        {
+            this.Sales = new HashSet<Sale>();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
         public long Id { get; set; }
         public string Name { get; set; }
         public string Cnpj { get; set; }
         public bool Active { get; set; }
-        public ICollection<Sale> Sales { get; set; }
-
+        public virtual ICollection<Sale> Sales { get; set; }
     }
 }
--- Backend/ProjetoLogistic/Domain/Entities/Sale.cs
+++ Backend/ProjetoLogistic/Domain/Entities/Sale.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +10,16 @@ namespace Domain.Entities
 {
     public class Sale
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
         public long Id { get; set; }
         public string Description { get; set; }
         public double Value { get; set; }
         public bool Active { get; set; }
         public DateTime DateCreated { get; set; }
-        public Company Company { get; set; }
+        public long CompanyId { get; set; }
+
+        [ForeignKey(nameof(CompanyId))]
+        public Company? Company { get; set; } = null!;
     }
 }

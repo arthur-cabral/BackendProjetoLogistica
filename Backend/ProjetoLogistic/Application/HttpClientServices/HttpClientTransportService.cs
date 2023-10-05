ec5623e3cc9309305c168e5a90f918cde432abc8
arthur-cabral <arthurmartins10052002@gmail.com>
2023-10-05 14:30:30 -0300

    chore: Adding HttpClient to comunicate with microservices

---
+++ Backend/ProjetoLogistic/Application/HttpClientServices/HttpClientTransportService.cs
@@ -0,0 +1,35 @@
+﻿using Application.DTO.Sale;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Application.HttpClientServices
+{
+    public class HttpClientTransportService : IHttpClientTransportService
+    {
+        private readonly HttpClient _client;
+        private readonly IConfiguration _configuration;
+
+        public HttpClientTransportService(HttpClient client, IConfiguration configuration)
+        {
+            _client = client;
+            _configuration = configuration;
+        }
+
+        public async void CreateTransportRequest(SaleDTO saleDTO)
+        {
+            var content = new StringContent
+                (
+                    JsonSerializer.Serialize(saleDTO),
+                    Encoding.UTF8,
+                    "application/json"
+                );
+
+            await _client.PostAsync(_configuration["TransportService"], content);
+        }
+    }
+}
---
+++ Backend/ProjetoLogistic/Application/HttpClientServices/IHttpClientTransportService.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTO.Sale;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.HttpClientServices
+{
+    public interface IHttpClientTransportService
+    {
+        public void CreateTransportRequest(SaleDTO saleDTO);
+    }
+}
